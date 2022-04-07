d7bf3819bb07ba9078c60ec8caea82f135a91439
aidonmccomb <101207078+aidonmccomb@users.noreply.github.com>
2022-04-07 13:41:07 -0400

    Initial Commit

---
+++ calcubot/calcubotApp.swift
@@ -0,0 +1,17 @@
+//
+//  calcubotApp.swift
+//  calcubot
+//
+//  Created by Student on 2022-04-07.
+//
+
+import SwiftUI
+
+@main
+struct calcubotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ calcubotUITests/calcubotUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  calcubotUITestsLaunchTests.swift
+//  calcubotUITests
+//
+//  Created by Student on 2022-04-07.
+//
+
+import XCTest
+
+class calcubotUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
