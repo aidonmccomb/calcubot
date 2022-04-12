3eeeec9e737c716fefb1b895c09c018e594154d1
aidonmccomb <101207078+aidonmccomb@users.noreply.github.com>
2022-04-12 12:49:48 -0400

    used in contentview

---
+++ calcubot/GearRatioScreen.swift
@@ -0,0 +1,21 @@
+//
+//  GearRatioScreen.swift
+//  calcubot
+//
+//  Created by Student on 2022-04-12.
+//
+
+import SwiftUI
+
+struct GearRatioScreen: View {
+    var body: some View {
+        Image("GearScreen")
+            .resizable()
+    }
+}
+
+struct GearRatioScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        GearRatioScreen()
+    }
+}
